ea0768deb53b05285bcbb7d352ea277357d90d13
Rajat Sharma <rajat.sharma@thesynergyworks.com>
2025-02-04 16:25:43 +0530

    UI updated

--- src/app/pages/category/categories/categories.component.ts
+++ src/app/pages/category/categories/categories.component.ts
@@ -15,19 +15,20 @@ import { CategoryDetailsComponent } from "../category-details/category-details.c
   styleUrl: './categories.component.scss',
 })
 export class CategoriesComponent {
+  pancake: string = 'assets/images/pancake.webp';
   CATEGORIES: Category[] = [
-    { id: 1, name: 'Pizzas', description: 'Italian hand-tossed pizzas', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 2, name: 'Desserts', description: 'Sweet treats and confections', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 3, name: 'Beverages', description: 'Refreshing drinks', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 4, name: 'Snacks', description: 'Quick bites and appetizers', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 5, name: 'Main Course', description: 'Hearty main dishes', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 6, name: 'Salads', description: 'Fresh and healthy salads', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 7, name: 'Pasta', description: 'Italian pasta dishes', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 8, name: 'Soups', description: 'Warm and comforting soups', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 9, name: 'Sandwiches', description: 'Fresh sandwiches', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 10, name: 'Breakfast', description: 'Morning favorites', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 11, name: 'Asian', description: 'Asian cuisine', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() },
-    { id: 12, name: 'Mexican', description: 'Mexican specialties', imageUrl: 'assets/images/pancake.svg', thumbnailUrl: 'assets/images/pancake.svg', createdAt: new Date(), updatedAt: new Date() }
+    { id: 1, name: 'Pizzas', description: 'Italian hand-tossed pizzas', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 2, name: 'Desserts', description: 'Sweet treats and confections', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 3, name: 'Beverages', description: 'Refreshing drinks', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 4, name: 'Snacks', description: 'Quick bites and appetizers', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 5, name: 'Main Course', description: 'Hearty main dishes', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 6, name: 'Salads', description: 'Fresh and healthy salads', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 7, name: 'Pasta', description: 'Italian pasta dishes', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 8, name: 'Soups', description: 'Warm and comforting soups', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 9, name: 'Sandwiches', description: 'Fresh sandwiches', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 10, name: 'Breakfast', description: 'Morning favorites', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 11, name: 'Asian', description: 'Asian cuisine', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() },
+    { id: 12, name: 'Mexican', description: 'Mexican specialties', imageUrl: this.pancake, thumbnailUrl: this.pancake, createdAt: new Date(), updatedAt: new Date() }
   ];
   private dialog = inject(MatDialog);
 
